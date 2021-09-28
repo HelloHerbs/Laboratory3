72ae14174cd71d0c73152d41815d670950797246
HelloHerbs <cantosherbie@gmail.com>
2021-09-28 04:07:33 +0000

    This is the final program for group collaboration

--- group1.py
+++ group1.py
@@ -4,10 +4,23 @@ work_hours = float(input ("Number of Total Work Hours: "))
 print("")
 salary = 60 * work_hours
 print("Gross Salary: Php " + format(salary, ",.2f"))
-if salary > 50000:
+if salary >= 50000:
     tax = 0.20 * salary
-    net = salary - tax   
-else:
+    net = salary - tax
+    
+elif salary >= 30000 and salary <= 49999:
+    tax = 0.15 * salary
+    net = salary - tax
+    
+elif salary >= 20000 and salary <= 29999:
+    tax = 0.10 * salary
+    net = salary - tax
+    
+elif salary >= 10000 and salary <= 19999:
+    tax = 0.05 * salary
+    net = salary - tax
+    
+elif salary < 10000:
     net = salary
     tax = 0.00
 
